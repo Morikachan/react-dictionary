4fc8829299b805982f5c1447f73a83d121411299
✿ アレクサンドラ ✿ <zuzukochkame@gmail.com>
2022-09-29 15:50:43 +0900

    api integration with axios, displaying results: part of speech, meaning (definition+example) after searching for word

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 import Form from "react-bootstrap/Form";
 
 function Dictionary() {
-  const [keyword, setKeyword] = useState("sunset");
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
+
+  function apiHundle() {
+    const apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiURL).then(handleResponse);
+  }
 
   function search(event) {
     event.preventDefault();
     console.log(keyword);
+    apiHundle();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   return (
@@ -25,6 +39,7 @@ function Dictionary() {
             onChange={handleKeywordChange}
           />
         </Form>
+        <Results results={results} />
       </div>
     </div>
   );
